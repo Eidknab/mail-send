be99f86675ad6da7b0a7e3f9b432d33ce64daaef
Eidknab <42701747+Eidknab@users.noreply.github.com>
2018-09-01 03:55:45 +0200

    Add files via upload

---
+++ mail_send.py
@@ -0,0 +1,28 @@
+﻿import smtplib
+
+def mail_send(mail_user, mail_password, mail_to, mail_message, smtp_server, smtp_port):
+	try:
+		server = smtplib.SMTP(smtp_server, smtp_port)
+		server.starttls()
+		server.login(mail_user, mail_password)
+		server.sendmail(mail_user, mail_to, mail_message)
+		server.quit()
+		done()
+	except:
+		failed()
+
+def done():
+	print('Done!')
+
+def failed():
+	print('failed!')
+	
+mail_user = ''
+mail_password = ''
+mail_to = ''
+mail_message = 'Salut, ceci est un message en Python!'
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+mail_send(mail_user, mail_password, mail_to, mail_message, smtp_server, smtp_port)
+	
---
+++ mail_send2.py
@@ -0,0 +1,51 @@
+﻿import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+def mail_send(mail_user, mail_password, mail_to, mail_subject, mail_body, filename, smtp_server, smtp_port):
+	try:
+		attachment = open(filename, 'rb')
+
+		mail_message = MIMEMultipart()
+		mail_message['From'] = mail_user
+		mail_message['To'] = mail_to
+		mail_message['Subject'] = mail_subject
+		mail_message.attach(MIMEText (mail_body, 'plain'))
+
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+		mail_message.attach(part)
+		mail_message = mail_message.as_string()
+
+		server = smtplib.SMTP(smtp_server, smtp_port)
+		server.starttls()
+		server.login(mail_user, mail_password)
+		server.sendmail(mail_user, mail_to, mail_message)
+		server.quit()
+		done()
+	except:
+		failed()
+		
+def done():
+	print('Done!')
+
+def failed():
+	print('failed!')
+	
+mail_user = ''
+mail_password = ''
+mail_to = ''
+mail_subject = 'Python!'
+mail_body = 'Salut, ceci est un message en Python!'
+filename = 'file1.txt'
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+mail_send(mail_user, mail_password, mail_to, mail_subject, mail_body, filename, smtp_server, smtp_port)
+
+	
